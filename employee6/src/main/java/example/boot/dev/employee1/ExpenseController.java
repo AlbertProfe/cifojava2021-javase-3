fefd15f39256797d7bd851cedacb8bbba6da270a
admin CIFO <admin CIFO@DESKTOP-8S89R17>
2021-06-23 13:16:31 +0200

    w3-panel plus templates

--- employee6/src/main/java/example/boot/dev/employee1/ExpenseController.java
+++ employee6/src/main/java/example/boot/dev/employee1/ExpenseController.java
@@ -1,9 +1,13 @@
 package example.boot.dev.employee1;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 	@Controller
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 		@Autowired
 		ExpenseRepository expenseRepository;
 
+		//----------------------- read ---------------------------------
 		@RequestMapping("/allExpenses")
 		public String getAllEmployees(Model boxToView) {
 			
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 			return "expenses.html";
 		}
 		
+		//-----------------------add----------------------------------
 		@RequestMapping("/newExpense")
 		public String newExpense () {
 
@@ -28,12 +34,100 @@ import org.springframework.web.bind.annotation.RequestMapping;
 		}
 
 		@RequestMapping("/addExpense")
-		public String inserEmployee( @Validated Expense expense) {
+		public String inserExpense ( @Validated Expense expense, Model boxToView) {
 			
 			//System.out.println(expense);
 			expenseRepository.save(expense);
+			boxToView.addAttribute("expensesfromController", expenseRepository.findAll());
+			boxToView.addAttribute("expenseAdded", expense);
 
-			return "redirect:/expense/allExpenses";
+			return "expenses.html";
 		}
+		
+		
+		//-----------------------delete----------------------------------
+		@RequestMapping("/deleteExpense")
+		public String removeExpense(int id, Model boxToView) {
+
+			// System.out.println("inside removeEmployee" + id);
+			Optional<Expense> expenseFound = findOneExpenseById(id);
+
+			// System.out.println("find inside removeEmployee" + employeeFound.get());
+
+			if (expenseFound.isPresent()) {
+
+				expenseRepository.deleteById(id);
+				
+				boxToView.addAttribute("expensesfromController", expenseRepository.findAll());
+				boxToView.addAttribute("expenseDeleted", expenseFound.get());
+				
+			}
+
+			else {
+				boxToView.addAttribute("expenseDeleted", "error");
+			}
+
+			// System.out.println("finishing removeEmployee" + id);
+			return "expenses.html";
+		}
+
+		
+		//-----------------------update----------------------------------
+		@RequestMapping("/updateExpense")
+		public String updateExpense(int id, Model model) {
+
+			Optional<Expense> expenseFound = findOneExpenseById(id);
+
+			if (expenseFound.isPresent()) {
+
+				model.addAttribute("expensefromController", expenseFound.get());
+				return "updateexpense.html";
+			}
+
+			else
+				return "notfound.html";
+		}
+
+		@PostMapping("/replaceExpense/{idFromView}")
+		public String replaceEmployee(@PathVariable("idFromView") int id, Expense expense, Model model) {
+
+			Optional<Expense> expenseFound = findOneExpenseById(id);
+
+			if (expenseFound.isPresent()) {
+
+				if (expense.getName() != null)
+					expenseFound.get().setName(expense.getName());
+				
+				
+				if (expense.getValue() != 0.0)
+					expenseFound.get().setValue(expense.getValue());
+
+				expenseRepository.save(expenseFound.get());
+				model.addAttribute("expensesfromController", expenseRepository.findAll());
+				model.addAttribute("expenseUpdated", expenseFound.get());
+				
+				return "expenses.html";
+
+			} else
+				return "notfound.html";
+
+		}
+		
+		
+	//--------------------------------------------------------------------------------
+	//------------------------- service to controller --------------------------------
+	//--------------------------------------------------------------------------------
+
+		public Optional<Expense> findOneExpenseById(int id) {
+
+			// System.out.println("inside findEmployee" + id);
+			Optional<Expense> expenseFound = expenseRepository.findById(id);
+			// System.out.println("finishing findEmployee" + id);
+			// System.out.println("finishing findEmployee" + employeeFound.get());
+			return expenseFound;
+		}
+		
+
+
 
 }
